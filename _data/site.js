af8100320c63a2044f5e93ef8649ae4600972bc5
Richard Herbert <richard@infoweb.co.uk>
2020-02-29 16:44:47 +0000

    Removed api variabel

--- _data/site.js
+++ _data/site.js
@@ -3,22 +3,18 @@ let url;
 switch ( process.env.ELEVENTY_ENV ) {
 	case 'production':
 		url = 'https://www.mastersbasketballtournaments.com/';
-		api = 'https://api.mastersbasketballtournaments.com/v1/';
 	break;
 
 	case 'development':
 		url = 'http://localhost:8080/';
-		api = 'http://api.mastersbasketballtournaments.localtest.me/v1/';
 	break;
 
 	default:
 		url = '/';
-		api = '/';
 }
 
 module.exports = {
 	url: url
-	,api: api
 	,title: 'Masters Basketball Tournaments'
 	,environment: process.env.ELEVENTY_ENV
 	,timezone: 'UTC'
