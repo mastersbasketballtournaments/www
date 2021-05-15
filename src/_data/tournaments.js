6bec1954f6ba405f56029640d43b1a270e56f78a
Richard Herbert <richard@infoweb.co.uk>
2021-05-15 23:11:25 +0100

    Added continent to the display

--- src/_data/tournaments.js
+++ src/_data/tournaments.js
@@ -10,7 +10,7 @@ const client = sanityClient( {
 } );
 
 module.exports = async function() {
-	const query = '*[ _type == "tournament" && endDate > now() ] | order( startDate ) { continent->, ... }'
+	const query = '*[ _type == "tournament" && endDate > now() ] | order( startDate ) { ...,continent-> { id, name } }'
 
 	return await client.fetch( query, {} )
 		.then( response => {
