28cdd33997cb970102d815d9e5f27e93f676e8b5
Richard Herbert <richard@infoweb.co.uk>
2024-08-05 17:20:01 +0100

    Removed Sanity

--- src/_data/seasons.js
+++ src/_data/seasons.js
@@ -22,29 +22,3 @@ module.exports = async function () {
 		}
 	} );
 };
-
-
-
-/*
-const sanityClient = require( '@sanity/client' )
-const client = sanityClient( {
-	projectId: process.env.SANITY_PROJECTID,
-	dataset: process.env.SANITY_DATASET,
-	token: process.env.SANITY_TOKEN,
-	apiVersion: '2021-03-25',
-	useCdn: false
-} );
-
-module.exports = async function() {
-	const query = '*[ _type == "tournament" && endDate > now() ] | order( startDate ) { "season": startDate }'
-
-	return await client.fetch( query, {} )
-		.then( response => {
-			return response.map( record => {
-				return { season: record.season.slice( 0, 4 ) };
-			} );
-		} )
-		.catch(
-			error => console.error( error )
-		);
-}
