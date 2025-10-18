2653e5de8717fafe764260c7038ec50520598ba2
Richard Herbert <richard@infoweb.co.uk>
2025-10-18 15:15:29 +0100

    Added scheduled deployment via Netlify

---
+++ netlify/functions/scheduled-deploy.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+import { schedule } from '@netlify/functions';
+
+const BUILD_HOOK = 'https://api.netlify.com/build_hooks/68244410651fbd3cd691548f'
+
+// Schedules the handler function to run daily at 02:37
+const handler = schedule( '37 2 * * *', async () => {
+	await fetch( BUILD_HOOK, {
+		method: 'POST'
+	} ).then(response => {
+		console.log('Build hook response:', response);
+	} )
+
+	return {
+		statusCode: 200
+	}
+} )
+
+export { handler }
