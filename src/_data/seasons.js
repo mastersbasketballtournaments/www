07f40e9146e9fa81c3d7508b3a958da01c245407
Richard Herbert <richard@infoweb.co.uk>
2026-03-13 09:55:55 +0000

    Updated to admin source

--- src/_data/seasons.js
+++ src/_data/seasons.js
@@ -2,19 +2,27 @@ import 'dotenv/config';
 import Fetch from "@11ty/eleventy-fetch";
 
 export default async function () {
-	let url = 'https://cms.mastersbasketballtournaments.com/api/seasons/';
+	let url = 'https://admin.mastersbasketballtournaments.com/api/seasons/';
 
 	if ( process.env.ELEVENTY_ENV == 'development' ) {
-		url = 'http://cms.mastersbasketballtournaments.localhost/api/seasons/';
+		url = 'http://localhost:5173/api/seasons/';
 	}
 
-	return Fetch( url, {
-		duration: '0d'
-		,type: 'json'
-		,fetchOptions: {
-			headers: {
-				'Authorization': process.env.API_TOKEN
+	try {
+		let dataset = await Fetch( url, {
+			duration: '0d'
+			,type: 'json'
+			,fetchOptions: {
+				headers: {
+					'Authorization': process.env.API_TOKEN
+				}
 			}
-		}
-	} );
+		} );
+
+		return dataset;
+	} catch ( event ) {
+		console.warn("Fetch failed, SEASONS returning empty fallback", event.message);
+
+		return [];
+	}
 };
--- src/_data/tournaments.js
+++ src/_data/tournaments.js
@@ -2,19 +2,27 @@ import 'dotenv/config'
 import Fetch from "@11ty/eleventy-fetch";
 
 export default async function () {
-	let url = 'https://cms.mastersbasketballtournaments.com/api/tournaments/';
+	let url = 'https://admin.mastersbasketballtournaments.com/api/tournaments/';
 
 	if ( process.env.ELEVENTY_ENV == 'development' ) {
-		url = 'http://cms.mastersbasketballtournaments.localhost/api/tournaments/';
+		url = 'http://localhost:5173/api/tournaments/';
 	}
 
-	return Fetch( url, {
-		duration: '0d'
-		,type: 'json'
-		,fetchOptions: {
-			headers: {
-				'Authorization': process.env.API_TOKEN
+	try {
+		let dataset = await Fetch( url, {
+			duration: '0d'
+			,type: 'json'
+			,fetchOptions: {
+				headers: {
+					'Authorization': process.env.API_TOKEN
+				}
 			}
-		}
-	} );
+		} );
+
+		return dataset;
+	} catch ( event ) {
+		console.warn("Fetch failed, SEASONS returning empty fallback", event.message);
+
+		return [];
+	}
 };
